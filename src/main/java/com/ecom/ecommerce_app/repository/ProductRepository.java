62452931ff554517cb3ba861dee21e3fdd04029c
Vivekdharan Muthupandi <vivekmuthupandi@gmail.com>
2026-03-16 10:16:51 +0530

    Update ProductRepository.java

--- src/main/java/com/ecom/ecommerce_app/repository/ProductRepository.java
+++ src/main/java/com/ecom/ecommerce_app/repository/ProductRepository.java
@@ -1,9 +1,7 @@
 package com.ecom.ecommerce_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ecom.ecommerce_app.entity.Product;
-
 public interface ProductRepository extends JpaRepository<Product, Long> 
 {
 	
